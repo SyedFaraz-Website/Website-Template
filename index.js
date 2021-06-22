0ad5791ba3d4d136614d02a7c5b90027ac086c61
SyedFaraz-Website <86298685+SyedFaraz-Website@users.noreply.github.com>
2021-06-22 04:24:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,333 @@
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    // document.getElementById("navbar").style.backgroundColor = "lightgray";
+    // document.getElementById("navbar").style.opacity = "0.5";
+    document.getElementById("navbar").style.textDecorationColor = "black";
+      
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "";;
+    document.getElementById("navbar").style.opacity = "";
+  }
+}
+
+
+
+
+
+
+
+function myFunction() {
+
+  document.getElementById("food").style.textDecorationColor = "blue";
+  document.getElementById("food").style.color = "blue";
+  document.getElementById("food").style.textDecoration = "underline";
+
+  document.getElementById("shoes").style.textDecorationColor = "";
+  document.getElementById("shoes").style.color = "";
+  document.getElementById("shoes").style.textDecoration = "";
+
+  document.getElementById("watches").style.textDecorationColor = "";
+  document.getElementById("watches").style.color = "";
+  document.getElementById("watches").style.textDecoration = "";
+
+  document.getElementById("cosmetics").style.textDecorationColor = "";
+  document.getElementById("cosmetics").style.color = "";
+  document.getElementById("cosmetics").style.textDecoration = "";
+
+
+  document.getElementById("phone").style.textDecorationColor = "";
+  document.getElementById("phone").style.color = "";
+  document.getElementById("phone").style.textDecoration = "";
+
+
+
+
+  document.getElementById("panal").style.color = "blue";
+  document.getElementById("panal").style.color = "blue";
+  document.getElementById("panal").style.display = "flex";
+  document.getElementById("panala").style.display = "flex";
+  document.getElementById("panal2").style.display = "none";
+  document.getElementById("panal2a").style.display = "none";
+  document.getElementById("panal3").style.display = "none";
+  document.getElementById("panal3a").style.display = "none";
+  document.getElementById("panal4").style.display = "none";
+  document.getElementById("panal4a").style.display = "none";
+  document.getElementById("panal5").style.display = "none";
+  document.getElementById("panal5a").style.display = "none";
+
+}
+function aaa() {
+
+
+  document.getElementById("food").style.textDecorationColor = "";
+  document.getElementById("food").style.color = "";
+  document.getElementById("food").style.textDecoration = "";
+
+  document.getElementById("watches").style.textDecorationColor = "";
+  document.getElementById("watches").style.color = "";
+  document.getElementById("watches").style.textDecoration = "";
+
+
+  document.getElementById("shoes").style.textDecorationColor = "blue";
+  document.getElementById("shoes").style.color = "blue";
+  document.getElementById("shoes").style.textDecoration = "underline";
+
+
+  document.getElementById("cosmetics").style.textDecorationColor = "";
+  document.getElementById("cosmetics").style.color = "";
+  document.getElementById("cosmetics").style.textDecoration = "";
+
+
+
+
+  document.getElementById("phone").style.textDecorationColor = "";
+  document.getElementById("phone").style.color = "";
+  document.getElementById("phone").style.textDecoration = "";
+
+
+
+  document.getElementById("panal2").style.display = "flex";
+  document.getElementById("panal2a").style.display = "flex";
+  document.getElementById("panal").style.display = "none";
+  document.getElementById("panala").style.display = "none";
+  document.getElementById("panal3").style.display = "none";
+  document.getElementById("panal3a").style.display = "none";
+  document.getElementById("panal4").style.display = "none";
+  document.getElementById("panal4a").style.display = "none";
+  document.getElementById("panal5").style.display = "none";
+  document.getElementById("panal5a").style.display = "none";
+
+}
+function bbb() {
+
+
+  document.getElementById("watches").style.textDecorationColor = "blue";
+  document.getElementById("watches").style.color = "blue";
+  document.getElementById("watches").style.textDecoration = "underline";
+
+  document.getElementById("food").style.textDecorationColor = "";
+  document.getElementById("food").style.color = "";
+  document.getElementById("food").style.textDecoration = "";
+
+  document.getElementById("shoes").style.textDecorationColor = "";
+  document.getElementById("shoes").style.color = "";
+  document.getElementById("shoes").style.textDecoration = "";
+
+  document.getElementById("cosmetics").style.textDecorationColor = "";
+  document.getElementById("cosmetics").style.color = "";
+  document.getElementById("cosmetics").style.textDecoration = "";
+
+
+
+
+
+  document.getElementById("phone").style.textDecorationColor = "";
+  document.getElementById("phone").style.color = "";
+  document.getElementById("phone").style.textDecoration = "";
+
+
+
+
+
+
+
+
+
+  document.getElementById("panal2").style.display = "none";
+  document.getElementById("panal2a").style.display = "none";
+  document.getElementById("panal").style.display = "none";
+  document.getElementById("panala").style.display = "none";
+  document.getElementById("panal3").style.display = "flex";
+  document.getElementById("panal3a").style.display = "flex";
+  document.getElementById("panal4").style.display = "none";
+  document.getElementById("panal4a").style.display = "none";
+  document.getElementById("panal5").style.display = "none";
+  document.getElementById("panal5a").style.display = "none";
+
+}
+function ccc() {
+
+
+  document.getElementById("food").style.textDecorationColor = "";
+  document.getElementById("food").style.color = "";
+  document.getElementById("food").style.textDecoration = "";
+
+  document.getElementById("shoes").style.textDecorationColor = "";
+  document.getElementById("shoes").style.color = "";
+  document.getElementById("shoes").style.textDecoration = "";
+
+  document.getElementById("watches").style.textDecorationColor = "";
+  document.getElementById("watches").style.color = "";
+  document.getElementById("watches").style.textDecoration = "";
+
+  document.getElementById("cosmetics").style.textDecorationColor = "blue";
+  document.getElementById("cosmetics").style.color = "blue";
+  document.getElementById("cosmetics").style.textDecoration = "underline";
+
+
+
+  document.getElementById("phone").style.textDecorationColor = "";
+  document.getElementById("phone").style.color = "";
+  document.getElementById("phone").style.textDecoration = "";
+
+
+
+
+
+
+  document.getElementById("panal2").style.display = "none";
+  document.getElementById("panal2a").style.display = "none";
+  document.getElementById("panal").style.display = "none";
+  document.getElementById("panala").style.display = "none";
+  document.getElementById("panal3").style.display = "none";
+  document.getElementById("panal3a").style.display = "none";
+  document.getElementById("panal4").style.display = "flex";
+  document.getElementById("panal4a").style.display = "flex";
+  document.getElementById("panal5").style.display = "none";
+  document.getElementById("panal5a").style.display = "none";
+
+}
+
+function ddd() {
+
+
+
+
+  document.getElementById("food").style.textDecorationColor = "";
+  document.getElementById("food").style.color = "";
+  document.getElementById("food").style.textDecoration = "";
+
+  document.getElementById("shoes").style.textDecorationColor = "";
+  document.getElementById("shoes").style.color = "";
+  document.getElementById("shoes").style.textDecoration = "";
+
+  document.getElementById("watches").style.textDecorationColor = "";
+  document.getElementById("watches").style.color = "";
+  document.getElementById("watches").style.textDecoration = "";
+
+  document.getElementById("cosmetics").style.textDecorationColor = "";
+  document.getElementById("cosmetics").style.color = "";
+  document.getElementById("cosmetics").style.textDecoration = "";
+
+
+
+  document.getElementById("phone").style.textDecorationColor = "blue";
+  document.getElementById("phone").style.color = "blue";
+  document.getElementById("phone").style.textDecoration = "underline";
+
+
+
+
+
+
+
+
+
+
+
+
+
+  document.getElementById("panal2").style.display = "none";
+  document.getElementById("panal2a").style.display = "none";
+  document.getElementById("panal").style.display = "none";
+  document.getElementById("panala").style.display = "none";
+  document.getElementById("panal3").style.display = "none";
+  document.getElementById("panal3a").style.display = "none";
+  document.getElementById("panal4").style.display = "none";
+  document.getElementById("panal4a").style.display = "none";
+  document.getElementById("panal5").style.display = "flex";
+  document.getElementById("panal5a").style.display = "flex";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('#recipeCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
